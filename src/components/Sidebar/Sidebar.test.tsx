92e4f63673565ab37623f1154f3d7ef6c0fb3665
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-03-27 16:31:25 -0300

    test: enhance Sidebar tests to include toggle functionality and additional menu item checks

--- src/components/Sidebar/Sidebar.test.tsx
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -1,11 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import Sidebar from './Sidebar';
-import '@testing-library/jest-dom';
-import React from 'react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import Sidebar from "./Sidebar";
+import "@testing-library/jest-dom";
+import React from "react";
 
-describe('Sidebar', () => {
-  test('renders the sidebar menu items', () => {
+describe("Sidebar", () => {
+  test("renders the sidebar menu items", () => {
     render(<Sidebar />);
+
     expect(screen.getByText(/SideBar Jest Test/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
+    expect(screen.getByText(/Mensagens/i)).toBeInTheDocument();
+    expect(screen.getByText(/Usuários/i)).toBeInTheDocument();
+    expect(screen.getByText(/Calendário/i)).toBeInTheDocument();
+    expect(screen.getByText(/Pesquisar/i)).toBeInTheDocument();
+    expect(screen.getByText(/Arquivos/i)).toBeInTheDocument();
+    expect(screen.getByText(/Configurações/i)).toBeInTheDocument();
   });
+
+  test("toggles the sidebar when the toggle button is clicked", () => {
+    render(<Sidebar />);
+
+    const toggleButton = screen.getByRole("button");
+    const sidebar = screen.getByTestId("sidebar-container");
+
+    expect(sidebar).toHaveStyle("width: 250px");
+
+    fireEvent.click(toggleButton);
+    expect(sidebar).toHaveStyle("width: 75px");
+
+    fireEvent.click(toggleButton);
+    expect(sidebar).toHaveStyle("width: 250px");
+  });
+
 });
