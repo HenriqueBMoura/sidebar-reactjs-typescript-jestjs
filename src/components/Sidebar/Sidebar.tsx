ca7afef6a5a90aeee5cb936c7f89beaf37c8809e
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-03-28 00:35:26 -0300

    refactor: reorganize Sidebar component structure and styling imports

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import {
+  SidebarContainer,
+  SidebarText,
+  ToggleButton,
+  MenuList,
+  MenuItemContainer,
+} from "./SidebarComponents";
 import {
   FaChartBar,
   FaEnvelope,
@@ -9,7 +15,6 @@ import {
   FaFolder,
   FaCog,
 } from "react-icons/fa";
-
 interface MenuItem {
   title: string;
   icon: React.ReactNode;
@@ -54,77 +59,19 @@ const menuList: MenuItem[] = [
   },
 ];
 
-const SidebarContainer = styled.div<{ $isOpen: boolean }>`
-	width: ${(props) => (props.$isOpen ? "250px" : "75px")};
-	height: 100vh;
-	padding: 20px 10px 20px 20px;
-	background-color: #3b0764;
-	color: #ffffff;
-	transition: width 0.5s ease-in-out;
-	position: relative;
-`;
-
-const ToggleButton = styled.button`
-	position: absolute;
-	top: 20px;
-	right: -20px;
-	width: 40px;
-	height: 40px;
-	color: #ffffff;
-	background-color: #160324;
-	border: 2px solid #ffffff;
-	border-radius: 50%;
-	cursor: pointer;
-
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
-
-const MenuList = styled.ul`
-	list-style: none;
-	padding: 0;
-	margin-top: 20px;
-`;
-
-const MenuItemContainer = styled.li`
-	display: flex;
-	align-items: center;
-	padding: 12px;
-	margin-top: 10px;
-	gap: 10px;
-	cursor: pointer;
-	border-radius: 8px;
-	transition: background-color 0.3s;
-
-	&:hover {
-		background-color: #ffffff;
-		color: #3b0764;
-	}
-		
-	svg {
-		font-size: 20px;
-	}
-`;
-
-const SidebarText = styled.span<{ $isOpen?: boolean }>`
-	display: ${(props) => (props.$isOpen ? "inline" : "none")};
-`;
-
 const Sidebar: React.FC = () => {
-
-	const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <SidebarContainer data-testid="sidebar-container" $isOpen={isOpen}>
       <ToggleButton onClick={() => setIsOpen(!isOpen)}>
-      	{isOpen ? "<" : ">"}	
-			</ToggleButton>
+        {isOpen ? "<" : ">"}
+      </ToggleButton>
       <MenuList>
         <h2>React Test</h2>
 
         {menuList.map((item) => (
           <MenuItemContainer key={item.title} role="img">
-						{item.icon}
+            {item.icon}
             <SidebarText $isOpen={isOpen}>{item.title}</SidebarText>
           </MenuItemContainer>
         ))}
