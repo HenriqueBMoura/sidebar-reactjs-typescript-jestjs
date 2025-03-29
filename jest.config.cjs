1b616bf63e461542d6c56e92b4ea974a9af2650b
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-03-29 04:08:03 -0300

    chore: update Jest configuration to include setup files and coverage options

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,30 @@ module.exports = {
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
   },
+  setupFilesAfterEnv: [
+    "<rootDir>/src/test/setup/jest.setup.ts"
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  // transform: {
+  //   "^.+\\.tsx?$": "ts-jest"
+  // },
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[tj]sx?$",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/test/"
+  ],
   testEnvironmentOptions: {
     html: '<html lang="zh-cmn-Hant"></html>',
     url: "https://jestjs.io/",
