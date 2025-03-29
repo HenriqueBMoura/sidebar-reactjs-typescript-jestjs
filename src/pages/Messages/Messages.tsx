edfb7bcfe97c949f4b012515b923cca2e0bbe8ca
Henrique Moura <henrique.bmoura@yahoo.com.br>
2025-03-29 04:09:31 -0300

    feat: add Dashboard and Messages components for user interface

---
+++ src/pages/Messages/Messages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Messages: React.FC = () => {
+  return (
+    <div>
+      <h1>Messages</h1>
+      <p>Your messages appear here.</p>
+    </div>
+  );
+};
+
+export default Messages;
